7dd5fcd0e61982d4b03a425af0925dfa5a422922
Xi Pu <xp19@duke.edu>
2020-09-22 17:18:15 -0700

    set up mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 let app = require('express')();
 let http = require('http').createServer(app);
 
+//Set up mongoose connection
+let mongoose = require('mongoose');
+/* use the test database if no environment variables named MONGODB_URI are passed in*/
+let mongoDB_URI = process.env.MONGODB_URI || 'mongodb+srv://xipu:k5q1J0qhOrVb1F65@cluster0.jcnnf.azure.mongodb.net/psych_game?retryWrites=true&w=majority'
+mongoose.connect(mongoDB_URI, { useNewUrlParser: true , useUnifiedTopology: true});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
