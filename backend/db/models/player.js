46a5ef7b275f8e64a54e2a36ea9d9b16ce62276f
Xi Pu <xp19@duke.edu>
2020-09-22 21:16:47 -0700

    add mongoose data schema

---
+++ backend/db/models/player.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+
+let PlayerSchema = new mongoose.Schema(
+    {
+        choices: {type: [String], default: [null]}, // TODO: still need to figure out what's the best way to store a player's choice at each turn
+        payment: {type: Number, default: 0}, // this player's final payment in cents
+    }
+);
+
+/* schema -> model -> document */
+// compile the above schema definition into a model and export it so we can use it elsewhere
+module.exports = {
+    PlayerSchema: PlayerSchema,
+    PlayerModel: mongoose.model('Player', PlayerSchema)
+};
