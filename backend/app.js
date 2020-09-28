58d0bfebee8162ac9b31d26f3debaad39af174ba
Xi Pu <xp19@duke.edu>
2020-09-28 09:40:09 -0700

    server static files from the React app

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,21 @@ app.use(function(req, res, next) {
     next();
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
 app.get("/test_api", (req, res) => {
     res.status(200).send("Hello World!");
 });
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 module.exports = app;
