812f8c314e9eecf9c2ae79bedfa5af29fa1ff5b2
Xi Pu <xp19@duke.edu>
2020-09-28 18:01:04 -0700

    require path module

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
