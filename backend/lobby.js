189fefe5e446cca69e65563d07db26f21a876769
Xi Pu <xp19@duke.edu>
2020-10-07 23:14:07 -0700

    add more helper functions in lobby.js to manage players and rooms

--- backend/lobby.js
+++ backend/lobby.js
@@ -1,37 +1,90 @@
 class Lobby {
-    nextRoomID = 0;
+    currRoomID = 0;
     rooms = [];
     currRoom;
+    playerToRoom = new Map(); // stores a mapping of a player's id to the room instance he is in
+    roomToPlayer = new Map();
     static MAX_CAPACITY_PER_ROOM = 6;
 
     constructor() {
-        let newRoom = new Room(`room ${this.nextRoomID}`);
-        this.rooms.push(newRoom);
-        this.currRoom = newRoom;
-    }
-
-    get availableRoomNameToJoin() {
-        return this.currRoom.name;
+        this.allocateNewRoom();
     }
 
     static getNumOfPeopleInRoom(serverSocket, roomName) {
         return serverSocket.sockets.adapter.rooms[roomName].length;
     }
 
-    allocateNewRoom(){
-        this.nextRoomID++;
-        this.currRoom = new Room(`room ${this.nextRoomID}`);
+    allocateNewRoom() {
+        this.currRoomID++;
+        this.currRoom = new Room(`room ${this.currRoomID}`);
         this.rooms.push(this.currRoom);
+        this.roomToPlayer.set(this.currRoom.name, []);
+    }
+
+    getRoomPlayerIsIn(prolificID){
+        return this.playerToRoom.get(prolificID);
+    }
+
+    getAllPlayersInRoomWithName(roomName){
+        return this.roomToPlayer.get(roomName);
+    }
+
+    findRoomForPlayerToJoin(prolificID){
+        if (this.playerToRoom.has(prolificID)) {
+            throw 'Duplicated prolificID found'
+        }
+        this.playerToRoom.set(prolificID, this.currRoom);
+        this.roomToPlayer.get(this.currRoom.name).push(new Player(prolificID));
+        return this.currRoom.name;
     }
 }
 
 class Room {
     constructor(room_name) {
+        if (room_name === undefined) {
+            throw 'Room name not defined';
+        }
         this.room_name = room_name;
+        this.turnNum = 1;
+        this.players = [];
     }
 
     get name() {
         return this.room_name;
+    }
+
+    addPlayer(player){
+        if (! player instanceof Player){
+            throw 'Parameter is not an instance of the Player class.';
+        }
+        this.players.push(player);
+    }
+
+    canFindPlayerWithID(prolificID){
+        let found = false;
+        this.players.forEach((player) => {
+            if (player.prolificID === prolificID){
+                found = true;
+            }
+        });
+        return found;
+    }
+}
+
+class Player {
+    constructor(prolificID) {
+        if (prolificID === undefined) {
+            throw 'prolificID not defined';
+        }
+        this.prolificID = prolificID;
+        this.isBot = false;
+    }
+
+    setIsBot(isBot){
+        if (typeof(isBot) !== 'boolean'){
+            throw 'Parameter is not a Boolean type.';
+        }
+        this.isBot = isBot;
     }
 }
 
@@ -39,9 +92,11 @@ const lobby = new Lobby();
 
 module.exports = {
     Lobby,
-    LobbySocketListener: function(io, socket) {
-        socket.on("enter lobby", () => {
-            let roomName = lobby.availableRoomNameToJoin;
+    Room,
+    Player,
+    LobbySocketListener: function (io, socket) {
+        socket.on("enter lobby", (prolificID) => {
+            let roomName = lobby.findRoomForPlayerToJoin(prolificID);
             socket.join(roomName);
             socket.roomName = roomName;
             socket.to(roomName).emit('join', socket.id + ' has joined ' + roomName);
