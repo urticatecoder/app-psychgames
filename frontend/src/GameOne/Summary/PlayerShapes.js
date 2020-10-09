460ae348a3ab002e63da67601d2d184d0637caad
Eric Doppelt <ead45@duke.edu>
2020-10-08 21:56:25 -0400

    added avatars to six players
    avatars are used in summary screen for issue #16

--- frontend/src/GameOne/Summary/PlayerShapes.js
+++ frontend/src/GameOne/Summary/PlayerShapes.js
@@ -1,14 +1,22 @@
 import {Rectangle, Circle, Ellipse, Line, Polyline, CornerBox, Triangle} from 'react-shapes';
 import React from 'react';
 
+const PATH_TO_AVATARS = "../../../"
+const WIZARD = "wizard.png"
 const PLAYER_ONE = (
-    <Rectangle
-    width={100}
-    height={100}
-    fill={{color:'#2409ba'}}
-    stroke={{color:'#E65243'}}
-    strokeWidth={3}
+    <img
+        src={PATH_TO_AVATARS + WIZARD}
+        width={100}
+        height={100}
     />
+        
+    // <Rectangle
+    // width={100}
+    // height={100}
+    // fill={{color:'#2409ba'}}
+    // stroke={{color:'#E65243'}}
+    // strokeWidth={3}
+    // />
 );
 
 const PLAYER_TWO = (
