83a121a77cbaeed3fe7d4690214154f6ac964f55
Xi Pu <xp19@duke.edu>
2020-10-17 22:51:33 -0700

    add admin page to test downloading mock data as csv

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,39 @@ app.get("/login-code", ((req, res) => {
     res.status(200).send({'isValid': isValid, 'error': error});
 }));
 
+app.get("/download", (req, res) => {
+    res.status(200).send("sjfioasjiodjsaiodjzxc");
+   // res.status(200).json([{
+   //      "id": 1,
+   //      "first_name": "Jeanette",
+   //      "last_name": "Penddreth",
+   //      "email": "jpenddreth0@census.gov",
+   //      "gender": "Female",
+   //      "ip_address": "26.58.193.2"
+   //  }, {
+   //      "id": 2,
+   //      "first_name": "Giavani",
+   //      "last_name": "Frediani",
+   //      "email": "gfrediani1@senate.gov",
+   //      "gender": "Male",
+   //      "ip_address": "229.179.4.212"
+   //  }, {
+   //      "id": 3,
+   //      "first_name": "Noell",
+   //      "last_name": "Bea",
+   //      "email": "nbea2@imageshack.us",
+   //      "gender": "Female",
+   //      "ip_address": "180.66.162.255"
+   //  }, {
+   //      "id": 4,
+   //      "first_name": "Willard",
+   //      "last_name": "Valek",
+   //      "email": "wvalek3@vk.com",
+   //      "gender": "Male",
+   //      "ip_address": "67.76.188.26"
+   //  }])
+});
+
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
 });
