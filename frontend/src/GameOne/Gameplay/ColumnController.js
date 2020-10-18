d7eecc03b1256e4b0c9b848e5aa60c7ddd790a61
Eric Doppelt <ead45@duke.edu>
2020-10-18 18:25:02 -0400

    created controller that moves each individual player in each column for Game One
    progress towards issue #72

--- frontend/src/GameOne/Gameplay/ColumnController.js
+++ frontend/src/GameOne/Gameplay/ColumnController.js
@@ -3,6 +3,11 @@ import '../../CommonStylings/FullScreenDiv.css'
 import PlayerColumn from '../Gameplay/PlayerColumn';
 import {Grid} from '@material-ui/core'
 
+const INITIAL_HEIGHT = 40;
+const NUM_PLAYERS = 6
+var heights = new Array(NUM_PLAYERS)
+heights.fill(INITIAL_HEIGHT)
+
 function ColumnController(props) {
     return (
         <div style={{backgroundColor: '#f00000'}}>
@@ -11,34 +16,31 @@ function ColumnController(props) {
                 direction="row"
                 justify='center'
                 spacing = {10}
+                style={{height: '80vh'}}
                 >
 
-                <Grid item>
-                    <PlayerColumn height={10} player={1}/>
-                </Grid>
-
-                <Grid item>
-                    <PlayerColumn height={20} player={2}/>
-                </Grid>
-
-                <Grid item>
-                    <PlayerColumn height={30} player={3}/>
-                </Grid>
-
-                <Grid item>
-                    <PlayerColumn height={0} player={4}/>
-                </Grid>
-
-                <Grid item>
-                    <PlayerColumn height={20} player={5}/>
-                </Grid>
-
-                <Grid item>
-                    <PlayerColumn height={10} player={6}/>
-                </Grid>
+                {getColumn(0)}
+                {getColumn(1)}
+                {getColumn(2)}
+                {getColumn(3)}
+                {getColumn(4)}
+                {getColumn(5)}
+                
             </Grid>
         </div>
     )
+}
+
+function getColumn(playerNumber) {
+    return(
+        <Grid item>
+            <PlayerColumn height={heights[playerNumber]} player={playerNumber}/>
+        </Grid>
+    )
+}
+
+function updateHeight(playerNum, newHeight) {
+    heights[playerNum] = newHeight;
 }
 
 export default (ColumnController);
