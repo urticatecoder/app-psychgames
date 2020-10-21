ce8482d02051ec404dd27b94ae578a345cc91b7a
nicolas.decapite <nicolas.decapite@duke.edu>
2020-10-20 21:45:10 -0400

    location calculated for each round works, need to store the current value to correctly update

--- backend/test/location_test.js
+++ backend/test/location_test.js
@@ -28,50 +28,49 @@ describe('Location sending and calculation', () => {
             assert(results[i] == 4);
         }
         done();
-        // DB_API.savePlayerChoiceToDB(testID[0], choices, num, bot).then(() => {
-        //     var results = getResultsByProlificId(testID, room);
-        //     console.log(results);
-        //     assert(results === [4, 4]);
-        //     done();
-        // }).catch((err) => {
-        //     done(err);
-        // });
     });
-    // it('ensuring the triple algorithm method works for true', (done) => {
-    //     const testID = 'test_id1';
-    //     saveNewPlayerToDB(testID);
-    //     var choices = ['test_id', 'test_id1'];
-    //     const num = 3;
-    //     const bot = false;
-    //     // why are there errors
-    //     let check = isTripleBonus('test1', 'test2', ['test1', 'test2', 'test3'], ['test1', 'test2', 'test3']);
-    //     assert(check === true);
-    //     done();
-    // });
+    it('calculates triple bonus correctly', (done) => {
+        const testID = ['test_id1', 'test_id2', 'test_id3'];
+        var choicesOne = ['test_id2', 'test_id3'];
+        var choicesTwo = ['test_id1', 'test_id3'];
+        var choicesThree = ['test_id1', 'test_id2'];
+        const num = 3;
+        const bot = false;
+        const room = new Room('room 0');
+        room.addPlayer(new Player('test_id1'));
+        room.addPlayer(new Player('test_id2'));
+        room.addPlayer(new Player('test_id3'));
+        room.getPlayerWithID('test_id1').recordChoices(choicesOne);
+        room.getPlayerWithID('test_id3').recordChoices(choicesThree);
+        room.getPlayerWithID('test_id2').recordChoices(choicesTwo);
 
-    // it('calculates results and location correctly', (done) => {
-    //     const testID = 'test_id1';
-    //     var choices = ['test_id', 'test_id1', 'test_id2'];
-    //     var choices_other = ['test_id1', 'test_id', 'test_id2'];
-    //     var other_choices = ['test_id2', 'test_id1', 'test_id'];
-    //     const num = 3;
-    //     const bot = false;
-    //     // why are there errors
-    //     const room = new Room('room 0');
-    //     room.addPlayer(new Player('test_id'));
-    //     room.addPlayer(new Player('test_id1'));
-    //     room.addPlayer(new Player('test_id2'));
-    //     room.getPlayerWithID('test_id').recordChoices(choices_other);
-    //     room.getPlayerWithID(testID).recordChoices(choices);
-    //     room.getPlayerWithID('test_id2').recordChoices(other_choices);
+        const count = getResultsByProlificId(testID, room);
+        console.log(count);
+        for(var i =0; i <count.length; i++){
+            assert(count[i] == 7);
+        }
+        done();
+    });
 
-    //     DB_API.savePlayerChoiceToDB(testID, choices, num, bot).then(() => {
-    //         const count = getResultsByProlificId(testID, num, room);
-    //         console.log(count);
-    //         assert(count === 80 );
-    //         done();
-    //     }).catch((err) => {
-    //         done(err);
-    //     });
-    // });
+    it('calculates triple bonus correctly', (done) => {
+        const testID = ['test_id1', 'test_id2', 'test_id3'];
+        var choicesOne = ['test_id2'];
+        var choicesTwo = ['test_id1'];
+        var choicesThree = ['test_id2'];
+        const num = 3;
+        const bot = false;
+        const room = new Room('room 0');
+        room.addPlayer(new Player('test_id1'));
+        room.addPlayer(new Player('test_id2'));
+        room.addPlayer(new Player('test_id3'));
+        room.getPlayerWithID('test_id1').recordChoices(choicesOne);
+        room.getPlayerWithID('test_id3').recordChoices(choicesThree);
+        room.getPlayerWithID('test_id2').recordChoices(choicesTwo);
+
+        const count = getResultsByProlificId(testID, room);
+        console.log(count);
+        assert(count[0] == 4 && count[1] == 4);
+        assert(count[2] == 0);
+        done();
+    });
 })
