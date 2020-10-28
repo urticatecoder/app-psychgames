669250bf6d7d7f547c1cf381c04d6f40097fde0d
nicolas.decapite <nicolas.decapite@duke.edu>
2020-10-27 20:40:12 -0400

    calculates all triple bonuses correctly and tested to prove

--- backend/db/results.js
+++ backend/db/results.js
@@ -8,10 +8,10 @@ function getResultsByProlificId(prolificIDArray, room) {
     let allLocations = room.playerLocation;
     let allResults = [];
 
-    for(let i = 0; i < prolificIDArray.length; i++){
+    for (let i = 0; i < prolificIDArray.length; i++) {
         let playerProlific = prolificIDArray[i];
-        let playerRoundResult = calculateDoubleAndTripleBonusesOfID( playerProlific, allChoices);
-        
+        let playerRoundResult = calculateDoubleAndTripleBonusesOfID(playerProlific, allChoices);
+
         let playerInitialLocation = allLocations.get(playerProlific);
         let newPlayerLocation = playerInitialLocation + playerRoundResult;
         room.setPlayerLocation(playerProlific, newPlayerLocation);
@@ -20,10 +20,10 @@ function getResultsByProlificId(prolificIDArray, room) {
     return allResults;
 }
 
-function calculateDoubleAndTripleBonusesOfID(playerProlific, allChoices){
+function calculateDoubleAndTripleBonusesOfID(playerProlific, allChoices) {
     let choicesProlific = allChoices.get(playerProlific);
     let count = 0;
-    for(var i = 0; i < choicesProlific.length; i++){
+    for (var i = 0; i < choicesProlific.length; i++) {
         let playerChosen = choicesProlific[i];
         let choicesChosenPlayer = allChoices.get(playerChosen);
         for (var j = 0; j < choicesChosenPlayer.length; j++) {
@@ -32,68 +32,67 @@ function calculateDoubleAndTripleBonusesOfID(playerProlific, allChoices){
             }
         }
     }
-    if(count === 30){
+    if (count === 30) {
         console.log('TRIPLE BONUS');
         return (count);
     }
     return count;
 }
 
-function calculateSingleBonusesOfID(playerProlific, allChoices, prolificIDArray){
+function calculateSingleBonusesOfID(playerProlific, allChoices, prolificIDArray) {
 
 }
 
-function calculateAllTripleBonuses(prolificIDArray, room){
+function calculateAllTripleBonuses(prolificIDArray, room) {
     let allChoices = room.getEveryoneChoiceAtCurrentTurn();
     let allTripleBonuses = [];
 
-    for(let i = 0; i < prolificIDArray.length; i++){
+    for (let i = 0; i < prolificIDArray.length; i++) {
         let playerProlific = prolificIDArray[i];
         let choicesProlific = allChoices.get(playerProlific);
         let tempTripleBonus = [];
-        if(choicesProlific.length == 2){
+        if (choicesProlific.length == 2) {
             let firstPlayerChosen = choicesProlific[0];
             let secondPlayerChosen = choicesProlific[1];
-            if(playerProlific == firstPlayerChosen || playerProlific == secondPlayerChosen){
-                let firstPlayerChoices = allChoices.get(firstPlayerChosen);
-                let secondPlayerChoices = allChoices.get(secondPlayerChosen);
-                if(firstPlayerChoices == 2 && secondPlayerChoices == 2){
-                    let triple = true;
-                    for(var k = 0; k < 2; k++){
-                        if(firstPlayerChoices[k] != playerProlific && firstPlayerChoices[k] != secondPlayerChosen){
-                            triple = false;
-                        }
-                        if(secondPlayerChoices[k] != playerProlific && secondPlayerChoices[k] != firstPlayerChosen){
-                            triple = false;
+            let firstPlayerChoices = allChoices.get(firstPlayerChosen);
+            let secondPlayerChoices = allChoices.get(secondPlayerChosen);
+            if (firstPlayerChoices.length == 2 && secondPlayerChoices.length == 2) {
+                let triple = true;
+                for (var k = 0; k < 2; k++) {
+                    if (firstPlayerChoices[k] != playerProlific && firstPlayerChoices[k] != secondPlayerChosen) {
+                        triple = false;
+                    }
+                    if (secondPlayerChoices[k] != playerProlific && secondPlayerChoices[k] != firstPlayerChosen) {
+                        triple = false;
+                    }
+                }
+                if (triple) {
+                    let duplicate = false;
+                    for (var j = 0; j < allTripleBonuses.length; j++) {
+                        if (allTripleBonuses[j].includes(playerProlific) || allTripleBonuses[j].includes(firstPlayerChosen
+                            || allTripleBonuses[j].includes(secondPlayerChosen))) {
+                            duplicate = true;
                         }
                     }
-                    if(triple){
-                        let duplicate = false;
-                        for(var j = 0; j<allTripleBonuses.length; j++){
-                            if(allTripleBonuses[j].includes(playerProlific) || allTripleBonuses[j].includes(firstPlayerChosen 
-                                || allTripleBonuses[j].includes(secondPlayerChosen))){
-                                    duplicate = true;
-                                }
-                        }
-                        if(!duplicate){
-                            tempTripleBonus.push(playerProlific);
-                            tempTripleBonus.push(firstPlayerChosen);
-                            tempTripleBonus.push(secondPlayerChosen);
-                            allTripleBonuses.push(tempTripleBonus);
-                        }
+                    if (!duplicate) {
+                        tempTripleBonus.push(playerProlific);
+                        tempTripleBonus.push(firstPlayerChosen);
+                        tempTripleBonus.push(secondPlayerChosen);
+                        allTripleBonuses.push(tempTripleBonus);
                     }
                 }
             }
+
         }
     }
     return allTripleBonuses;
 }
 
-function isGameOneDone(room){
+function isGameOneDone(room) {
     let allLocations = room.playerLocation;
     var playerMax = 0;
-    for(let location of allLocations.values()){
-        if(location >= 100){
+    for (let location of allLocations.values()) {
+        if (location >= 100) {
             playerMax += 1;
         }
     }
@@ -104,11 +103,11 @@ function getWinnersAndLosers(room) {
     let allLocations = room.playerLocation;
     let winners = [];
     let losers = [];
-    for(let tempPlayer of allLocations.keys()){
-        if(allLocations.get(tempPlayer) >= 100){
+    for (let tempPlayer of allLocations.keys()) {
+        if (allLocations.get(tempPlayer) >= 100) {
             winners.push(tempPlayer);
         }
-        else{
+        else {
             losers.push(tempPlayer);
         }
     }
@@ -117,7 +116,8 @@ function getWinnersAndLosers(room) {
 
 
 module.exports = {
-    getResultsByProlificId : getResultsByProlificId,
-    isGameOneDone : isGameOneDone,
+    getResultsByProlificId: getResultsByProlificId,
+    isGameOneDone: isGameOneDone,
     getWinnersAndLosers: getWinnersAndLosers,
+    calculateAllTripleBonuses: calculateAllTripleBonuses,
 }
--- backend/test/location_test.js
+++ backend/test/location_test.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const { saveNewPlayerToDB } = require('../db/db_api.js');
 const Room = require('../lobby.js').Room;
 const Player = require('../lobby.js').Player;
-const { getResultsByProlificId, isGameOneDone, getWinnersAndLosers } = require('../db/results.js');
+const { getResultsByProlificId, isGameOneDone, getWinnersAndLosers, calculateAllTripleBonuses } = require('../db/results.js');
 const DB_API = require('../db/db_api.js');
 
 
@@ -22,7 +22,7 @@ describe('Location sending and calculation', () => {
         let results = getResultsByProlificId(testID, room)
         console.log(results);
         for(var i = 0; i < results.length; i++ ){
-            assert(results[i] === 30);
+            assert(results[i] === 15);
         }
         done();
     });
@@ -62,12 +62,12 @@ describe('Location sending and calculation', () => {
 
         const count = getResultsByProlificId(testID, room);
         console.log(count);
-        assert(count[0] == 30 && count[1] == 30);
+        assert(count[0] == 15 && count[1] == 15);
         assert(count[2] == 0);
 
         const newRoundCount = getResultsByProlificId(testID, room);
         console.log(newRoundCount);
-        assert(newRoundCount[0] == 60 && newRoundCount[1] == 60);
+        assert(newRoundCount[0] == 30 && newRoundCount[1] == 30);
         assert(newRoundCount[2] == 0);
         done();
     });
@@ -92,6 +92,56 @@ describe('Location sending and calculation', () => {
     //     assert(isGameOneDone(room) == true);
     //     done();
     // });
+    it('calculates triple bonus', (done) => {
+        const testID = ['test_id1', 'test_id2', 'test_id3'];
+        var choicesOne = ['test_id2', 'test_id3'];
+        var choicesTwo = ['test_id1', 'test_id3'];
+        var choicesThree = ['test_id2', 'test_id1'];
+        const room = new Room('room 0');
+        room.addPlayer(new Player('test_id1'));
+        room.addPlayer(new Player('test_id2'));
+        room.addPlayer(new Player('test_id3'));
+        room.getPlayerWithID('test_id1').recordChoices(choicesOne);
+        room.getPlayerWithID('test_id3').recordChoices(choicesThree);
+        room.getPlayerWithID('test_id2').recordChoices(choicesTwo);
+
+        let tripleBonuses = calculateAllTripleBonuses(testID, room);
+        assert(tripleBonuses[0][0] == 'test_id1');
+        assert(tripleBonuses[0][1] == 'test_id2');
+        assert(tripleBonuses[0][2] == 'test_id3');
+        done();
+    });
+    it('calculates triple bonus with 2 triple bonuses', (done) => {
+        const testID = ['test_id1', 'test_id2', 'test_id3', 'test_id4', 'test_id5', 'test_id6'];
+        var choicesOne = ['test_id2', 'test_id3'];
+        var choicesTwo = ['test_id1', 'test_id3'];
+        var choicesThree = ['test_id2', 'test_id1'];
+        var choicesFour = ['test_id5', 'test_id6'];
+        var choicesFive = ['test_id6', 'test_id4'];
+        var choicesSix = ['test_id5', 'test_id4'];
+        const room = new Room('room 0');
+        room.addPlayer(new Player('test_id1'));
+        room.addPlayer(new Player('test_id2'));
+        room.addPlayer(new Player('test_id3'));
+        room.addPlayer(new Player('test_id4'));
+        room.addPlayer(new Player('test_id5'));
+        room.addPlayer(new Player('test_id6'));
+        room.getPlayerWithID('test_id1').recordChoices(choicesOne);
+        room.getPlayerWithID('test_id3').recordChoices(choicesThree);
+        room.getPlayerWithID('test_id2').recordChoices(choicesTwo);
+        room.getPlayerWithID('test_id4').recordChoices(choicesFour);
+        room.getPlayerWithID('test_id5').recordChoices(choicesFive);
+        room.getPlayerWithID('test_id6').recordChoices(choicesSix);
+
+        let tripleBonuses = calculateAllTripleBonuses(testID, room);
+        assert(tripleBonuses[0][0] == 'test_id1');
+        assert(tripleBonuses[0][1] == 'test_id2');
+        assert(tripleBonuses[0][2] == 'test_id3');
+        assert(tripleBonuses[1][0] == 'test_id4');
+        assert(tripleBonuses[1][1] == 'test_id5');
+        assert(tripleBonuses[1][2] == 'test_id6');
+        done();
+    });
 
 
 
