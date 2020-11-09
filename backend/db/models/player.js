49cf31b15ab3773586f265b61a29f491da510804
Xi Pu <xp19@duke.edu>
2020-11-08 17:27:54 -0800

    modify database schema

--- backend/db/models/player.js
+++ backend/db/models/player.js
@@ -1,10 +1,12 @@
 let mongoose = require('mongoose');
+let Choice = require('./choice');
 
 let PlayerSchema = new mongoose.Schema(
     {
+        _id: false,
         prolificID: {type: String, required: true},
-        game: {type: [String], default: [null]},
-        choice: {type: [String], default: [null]}, // TODO: still need to figure out what's the best way to store a player's choice at each turn
+        choice: {type: [Choice.ChoiceSchema], default: []}, // TODO: still need to figure out what's the best way to store a player's choice at each turn
+        allocation: {type: [Choice.ChoiceSchema], default: []},
         payment: {type: Number, default: 0}, // this player's final payment in cents
     }
 );
