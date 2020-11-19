b7d918443a09896fc1fde291e5c9e71fba565585
ericdoppelt <ead45@duke.edu>
2020-11-18 23:20:41 -0500

    refactored, formatted, and commented code in common_components and common_stylings

--- frontend/src/common_components/ContinueButton.js
+++ frontend/src/common_components/ContinueButton.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import {Button} from '@material-ui/core';
+import React from "react";
+import { Button } from "@material-ui/core";
 import { withRouter } from "react-router-dom";
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
+const BUTTON_MESSAGE = "Continue";
+const BUTTON_VARIANT = "contained";
+const BUTTON_COLOR = "primary";
 
-const BUTTON_MESSAGE = 'Continue';
+const styles = {
+  continueButton: {
+    width: "200px",
+    height: "50px",
+  },
+};
 
-const styles = ({
-    continueButton: {        
-        width: '200px',
-        height: '50px',
-    },
-});
-
+/**
+ * Styled button used through the UI that takes in a route and a boolean indicating whether the button should be disabled or not.
+ * Used for routing purposes mainly.
+ *
+ * @author Eric Doppelt
+ */
 function ContinueButton(props) {
+  const { classes } = props;
 
-    const {classes} = props;
-
-    return(
-        
-        <div>
-            <Button
-                className = {classes.continueButton}
-                variant="contained" 
-                color= {"primary"}
-                onClick={() => props.history.push(props.route)}
-                disabled={props.disabled}
-                >
-                {BUTTON_MESSAGE}
-            </Button>
-        </div>
-    )
+  return (
+    <Button
+      className={classes.continueButton}
+      variant={BUTTON_VARIANT}
+      color={BUTTON_COLOR}
+      onClick={() => props.history.push(props.route)}
+      disabled={props.disabled}
+    >
+      {BUTTON_MESSAGE}
+    </Button>
+  );
 }
 
 export default withStyles(styles)(withRouter(ContinueButton));
--- frontend/src/common_components/GameTimer.js
+++ frontend/src/common_components/GameTimer.js
@@ -1,77 +1,89 @@
-import React from 'react';
-import {Typography, withStyles} from '@material-ui/core';
-import Timer from 'react-compound-timer'
-
-const styles = ({
-    timerInstruction: {
-      marginTop: '50px',
-      position: 'absolute',
-      top: '22vh',
-      left: '5vw',
-      backgroundColor: '#349eeb',
-      height: '35vh',
-      width: '15vw',
-      opacity: '.8',
-      borderRadius: '20px',
-      alignItems: 'center',
-      verticalAlign: 'middle',
-    },
-    timerMargin: {
-        marginTop: '11vh'
-    }
-  });
+import React from "react";
+import { Typography, withStyles } from "@material-ui/core";
+import Timer from "react-compound-timer";
 
 const TURN_TIME = 30 * 1000;
-const LAST_TIME_UNIT = 'h';
-const DIRECTION = 'backward';
-const STOP_TIMER = 0;
+const LAST_TIME_UNIT = "h";
+const TIME_DIRECTION = "backward";
+const TIME_OVER = 0;
 const TIMER_UPDATE = 10;
 
-const SUBMIT_DECISIONS = true
+const SUBMIT_DECISIONS = true;
 
-const DO_NOT_RESET_TIMER = false
-const SECONDS_VARIANT = 'h1';
-const TIMER_MESSAGE = "The round ends in:"
+const RESET_TIMER = false;
+const SECONDS_VARIANT = "h1";
+const TIMER_MESSAGE = "The round ends in:";
 
+const styles = {
+  timerInstruction: {
+    marginTop: "50px",
+    position: "absolute",
+    top: "22vh",
+    left: "5vw",
+    backgroundColor: "#349eeb",
+    height: "35vh",
+    width: "15vw",
+    opacity: ".8",
+    borderRadius: "20px",
+    alignItems: "center",
+    verticalAlign: "middle",
+  },
+  timerMargin: {
+    marginTop: "11vh",
+  },
+};
+
+/**
+ * Timer used in Game One and Game Two that counts down from 30 seconds.
+ * When the timer ends, a function given as a prop to submit decisions is triggered.
+ * A prop is also given which, when true, will reset the timer. This occurs if all six players submit their choices before 30 seconds are up.
+ *
+ * @author Eric Doppelt
+ */
 function GameTimer(props) {
-    const {classes} = props
-    
-    return (
-        <div className={classes.timerInstruction}>
-            <Timer
-                initialTime={TURN_TIME}
-                lastUnit={LAST_TIME_UNIT}
-                direction={DIRECTION}
-                timeToUpdate={TIMER_UPDATE}
-                checkpoints={[
-                    {
-                        time: STOP_TIMER,
-                        callback: () => props.setSubmitDecisions(SUBMIT_DECISIONS),
-                    },
-                ]}
-            >
-                {({reset, start}) => (
-                    <div className={classes.timerMargin}>
-                        <React.Fragment>
-                            {checkForReset(reset, start, props.resetTimer, props.setResetTimer)}
-                            {TIMER_MESSAGE} 
-                            <Typography variant={SECONDS_VARIANT}> 
-                                <Timer.Seconds/> 
-                            </Typography>
-                        </React.Fragment>
-                    </div>
-                )}
-            </Timer>
-        </div>
-    )
+  const { classes } = props;
+
+  return (
+    <div className={classes.timerInstruction}>
+      <Timer
+        initialTime={TURN_TIME}
+        lastUnit={LAST_TIME_UNIT}
+        direction={TIME_DIRECTION}
+        timeToUpdate={TIMER_UPDATE}
+        checkpoints={[
+          {
+            time: TIME_OVER,
+            callback: () => props.setSubmitDecisions(SUBMIT_DECISIONS),
+          },
+        ]}
+      >
+        {({ reset, start }) => (
+          <div className={classes.timerMargin}>
+            <React.Fragment>
+              {checkForReset(
+                reset,
+                start,
+                props.resetTimer,
+                props.setResetTimer
+              )}
+              {TIMER_MESSAGE}
+              <Typography variant={SECONDS_VARIANT}>
+                <Timer.Seconds />
+              </Typography>
+            </React.Fragment>
+          </div>
+        )}
+      </Timer>
+    </div>
+  );
 }
 
 function checkForReset(reset, start, resetTimer, setResetTimer) {
-    if (resetTimer) {
-        reset()
-        setResetTimer(DO_NOT_RESET_TIMER)
-        start()
-    }
+  if (resetTimer) {
+    reset();
+    setResetTimer(RESET_TIMER);
+    start();
+  }
 }
 
 export default withStyles(styles)(GameTimer);
