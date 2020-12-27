bd3a6f8a501bcc041db95ab90d816db0604d7214
nicolasdecapite1 <nicolas.decapite@gmail.com>
2020-12-27 18:08:29 -0500

    added the socket call for time in lobby to emit prolific ID

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,14 @@ io.on('connection', socket => {
         require('./lobby.js').LobbyDefaultSocketListener(io, socket);
     }
 
+    socket.on('time in lobby', (prolificID) => {
+        if(prolificID != null){
+            let time = room.getTime(prolificID);
+            console.log(prolificID + ' time is' + time);
+            io.in(room.name).emit('player time', time);
+        }
+    })
+
     socket.on('confirm choice for game 1', (prolificID, choices, zeroTime) => {
         // prolific = prolific id; choices = [player1chosen, player2chosen] *minimum chosen players = 1*
         console.log(choices);
