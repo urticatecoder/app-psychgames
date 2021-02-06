1341804d0ee3301d817f374e65b22ba8c1ae968c
ericdoppelt <ead45@duke.edu>
2021-02-05 20:35:43 -0500

    formatted payout odds in game 2

--- frontend/src/util/common_components/RefreshChecker.js
+++ frontend/src/util/common_components/RefreshChecker.js
@@ -14,6 +14,7 @@ const LEAVE_MESSAGE = "If you leave, you won't be compensated."
 
     window.onbeforeunload = function () {return LEAVE_MESSAGE;}
     // DELETE THIS WHEN READY
+    return null;
     if (props.loginCode == null && props.location.pathname != HOME_ROUTE) return <Redirect to={HOME_ROUTE}/>
     return null;
   }
