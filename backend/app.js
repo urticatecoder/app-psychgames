772deb6241b3b586e86e6f4f84532551a8045f7e
Hsingchih Tang <ht114@duke.edu>
2021-04-02 17:16:08 -0400

    Updated node packages

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.get("/login-code", ((req, res) => {
         isValid = false;
         error = 'ProlificID is empty.';
     } else if (prolificID === 'CS307') { // for testing purpose
-        isValid = false;
+        isValid = true;
         error = 'Not a code registered by Prolific.'
     } else if (lobby.playerToRoom.has(prolificID)) {
         isValid = false;
