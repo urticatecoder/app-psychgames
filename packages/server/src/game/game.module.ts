6e5a845783084cbf489fb2803b674cfb01e42681
Dominic Martinez <dominic.martinez@duke.edu>
2021-09-18 14:59:33 -0400

    fix(server): correctly provide GameFactoryService in module

--- packages/server/src/game/game.module.ts
+++ packages/server/src/game/game.module.ts
@@ -1,9 +1,17 @@
 import { Module } from "@nestjs/common";
 import { GameGateway } from "./controllers/game/game.gateway";
+import { GameFactory } from "./services/game-factory/game-factory";
+import { GameFactoryService } from "./services/game-factory/game-factory.service";
 import { GameManagerService } from "./services/game-manager/game-manager.service";
 
 @Module({
   controllers: [GameGateway],
-  providers: [GameManagerService],
+  providers: [
+    GameManagerService,
+    {
+      provide: GameFactory,
+      useClass: GameFactoryService,
+    },
+  ],
 })
 export class GameModule {}
