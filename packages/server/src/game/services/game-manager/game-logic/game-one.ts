ab286857e7a4d5be5225807e6e2afd14604e373f
Dominic Martinez <dominic.martinez@duke.edu>
2021-09-20 13:16:16 -0400

    feat(server/game-one): scaffold game one functionality

--- packages/server/src/game/services/game-manager/game-logic/game-one.ts
+++ packages/server/src/game/services/game-manager/game-logic/game-one.ts
@@ -1,16 +1,47 @@
-import { GameOneModel } from "@dpg/types";
+import { GameOneModel, GameModel, PlayerModel } from "@dpg/types";
 import { AGame, GameInstance } from "./game.js";
 
 export class GameOne implements GameInstance {
+  private selections: Map<PlayerModel.ID, Set<PlayerModel.ID>>;
+
   public state: GameOneModel.State;
+
   constructor(private game: AGame) {
-    throw new Error("Method not implemented.");
+    this.selections = new Map();
+    const playerPositions: GameOneModel.PlayerPosition[] = [];
+    game.players.forEach((player: GameModel.Player) => {
+      playerPositions.push({
+        player,
+        position: 0.5,
+      });
+    });
+
+    this.state = {
+      type: "game-one",
+      round: 0,
+      // will be set on round start
+      roundStartTime: new Date(),
+      roundEndTime: new Date(),
+      playerPositions,
+    };
+
+    this.beginRound();
   }
 
   submitAction(playerID: string, action: GameOneModel.Turn): void {
-    throw new Error("Method not implemented.");
+    this.selections.set(
+      playerID,
+      new Set(
+        action.playersSelected.map((player: GameModel.Player) => player.id)
+      )
+    );
   }
-  getState(): GameOneModel.State {
-    throw new Error("Method not implemented.");
+
+  beginRound() {
+    throw new Error("Method not implemented");
+  }
+
+  advanceRound() {
+    throw new Error("Method not implemented");
   }
 }
