4aa237318a2293ec4d746dc59e6bc8624918d62c
Dominic Martinez <dominic.martinez@duke.edu>
2021-11-08 12:19:22 -0500

    feat(server/game-one): modify constants to address double vs. triple
    Doubles and triples are now disjoint bonuses, so the default values need to ensure triples actually get more value.

--- packages/game-server/src/game/services/game-manager/constants.ts
+++ packages/game-server/src/game/services/game-manager/constants.ts
@@ -4,34 +4,37 @@
 export type GameConstants = {
   lobbyTime: number;
   // round times are allowed to vary round to round
-  gameOne: {
-    roundTime: (round: number) => number;
-    maxRounds: number;
-    positionChange: {
-      single: (round: number, position: number) => number;
-      double: (round: number, position: number) => number;
-      triple: (round: number, position: number) => number;
-    };
+  gameOne: GameOneConstants;
+};
+
+export type GameOneConstants = {
+  roundTime: (round: number) => number;
+  maxRounds: number;
+  positionChange: {
+    single: (round: number, position: number) => number;
+    double: (round: number, position: number) => number;
+    triple: (round: number, position: number) => number;
   };
 };
 
 const bias = (position: number) => position * 0.3;
+const s = 1000;
 
 export const DefaultGameConstants: GameConstants = {
-  lobbyTime: 120 * 1000,
+  lobbyTime: 120 * s,
   gameOne: {
     roundTime: (round: number) => {
-      if (round === 0) return 30 * 1000;
-      if (round === 1) return 20 * 1000;
-      else return 15 * 1000;
+      if (round === 0) return 30 * s;
+      if (round === 1) return 20 * s;
+      else return 15 * s;
     },
     maxRounds: 10,
     // TODO: change these to something sensible
     positionChange: {
       // we arbitrarily bias position by 30% of their distance from the origin
       single: (round, position) => 0.05 * round * round + 0.1 + bias(position),
-      double: (round, position) => 0.05 * round * round + 0.2 + bias(position),
-      triple: (round, position) => 0.05 * round * round + 0.35 + bias(position),
+      double: (round, position) => 0.05 * round * round + 0.1 + bias(position),
+      triple: (round, position) => 0.15 * round * round + 0.35 + bias(position),
     },
   },
 };
