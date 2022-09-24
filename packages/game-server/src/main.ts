92721f08599cc51b6da67dbf4e81ea92e5f2d884
Ben Li <benjamin.li@duke.edu>
2022-09-24 15:21:18 -0400

    changed server port to 3001

--- packages/game-server/src/main.ts
+++ packages/game-server/src/main.ts
@@ -3,6 +3,6 @@ import { GameModule } from "./game/game.module.js";
 
 async function bootstrap() {
   const app = await NestFactory.create(GameModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
