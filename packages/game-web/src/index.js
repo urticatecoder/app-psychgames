52aa90abcd180cc42ea78963b7aab37377008770
Ben Li <benjamin.li@duke.edu>
2022-10-04 23:21:02 -0400

    Imported Router and history, wrapped App in Router with history prop

--- packages/game-web/src/index.js
+++ packages/game-web/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createBrowserHistory } from 'history';
+const history = createBrowserHistory();
+
 ReactDOM.render(
   <React.StrictMode>
+    <Router history={history}>
       <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
