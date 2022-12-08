38be3907660dea3591a7c63d70922277461f14b5
Ben Li <benjamin.li@duke.edu>
2022-12-08 14:42:10 -0500

    Different background color if id matches player's own id

--- packages/game-web/src/icons/components/PlayerProfile.js
+++ packages/game-web/src/icons/components/PlayerProfile.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import getImage from './getImage';
+import { withStyles } from "@material-ui/core";
+
 const LABEL = 'label'
+const SELF = "#faf3b1";
+const BORDER_RADIUS = 30;
 
 /**
  * Component used to visualize avatars with a label indicating the avatar's name beneath it.
@@ -10,11 +14,34 @@ const LABEL = 'label'
  * @author Eric Doppelt
  */
 function PlayerProfile(props) {
-    return(
-        <div>
-        {getImage(props.player, props.selectedIndex, props.frontendIndex)}
-        </div>
-    )
+    // find avatar index using player id and player data
+    // console.log("player id: ", props.playerId);
+    // console.log("player data: ", props.playerData);
+    // console.log("self id: ", props.id);
+    var avatarIndex = 0;
+    var isSelf = false;
+    for (var i = 0; i < props.playerData.length; i++) {
+        if (props.playerId == props.playerData[i].id) {
+            avatarIndex = props.playerData[i].avatar;
+        }
+    }
+    if (props.playerId == props.id) {
+        return(
+            <div
+            style={{ backgroundColor: SELF, borderRadius: BORDER_RADIUS }}
+            >
+                <div>
+                    {getImage(avatarIndex, props.selectedIndex, props.frontendIndex, isSelf)}
+                </div>
+            </div>
+        )
+    } else {
+        return(
+            <div>
+                {getImage(avatarIndex, props.selectedIndex, props.frontendIndex, isSelf)}
+            </div>
+        )
+    }    
 }
 
 
